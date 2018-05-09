e5b17d40d58f7dd20f6b306fdb64315045606ba3
David Huarng <davidhuarng@Davids-MacBook-Pro.local>
2018-05-09 20:37:39 +0800

    problem

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+# url(r'/index1/^$', views.index1, name='index1'),
 url(r'^$', views.index, name='index')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import subprocess
 import os
-
+# def index1(request):
+#     return HttpResponse(os.system('''python scripts/fetch.py'''))
+# need to make this function work
 def index(request):
     #return HttpResponse("<h2>HEY!<h2>")
     # return HttpResponse(os.system(''' python fetch.py'''))
-    return HttpResponse(os.system('''python2 -m scripts.label_image     --graph=tf_files/retrained_graph.pb      --image=scripts/0.jpg
+    return HttpResponse(os.system('''python scripts/fetch.py && python2 -m scripts.label_image     --graph=tf_files/retrained_graph.pb      --image=0.jpg
         '''))
       #return HttpResponse(subprocess.Popen(["python","tf_files/test1.py"],close_fds=True))
